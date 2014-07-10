7b52d7ca60fc69b2a7a71d445780b68e73454e48
Tim Duckett <tim@timduckett.co.uk>
2014-07-10 11:26:48 +0200

    Initial Commit

---
+++ CVInsertion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CVInsertion
+//
+//  Created by Tim on 10/07/14.
+//  Copyright (c) 2014 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CVInsertion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CVInsertion
+//
+//  Created by Tim on 10/07/14.
+//  Copyright (c) 2014 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
