b1e65680b61f6c41c9936cc213843d1f4865b166
Tim Duckett <tim@timduckett.co.uk>
2014-07-11 17:07:33 +0200

    Updates comments and generally tidies up

--- CVInsertion/CustomLayout.m
+++ CVInsertion/CustomLayout.m
@@ -95,25 +95,32 @@
     
     // Check if this indexPath appears in the list of index paths being updated.
     // If it doesn't, we can don't need to setup the center point
+    
     NSIndexSet *indexSet = [self.indexPathsBeingUpdated indexesOfObjectsPassingTest:^BOOL(UICollectionViewUpdateItem *updateItem, NSUInteger idx, BOOL *stop) {
             return (updateItem.indexPathAfterUpdate.row == itemIndexPath.row);
         }];
 
-    // This item isn't one that's appearing, therefore we only need to set the alpha attribute
+    // This item isn't one that's appearing, therefore we can use the attributes provided
+    // by the superclass method
+    
     if ([indexSet count] == 0) {
         return [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
     }
 
     // Test to see if we're dealing with a situation where we're removing
-    // the second item - then the first item needs to start where it originated,
-    // at the top
+    // the second item - there will now only be 1 item, and the indexPath.row that we're
+    // dealing with will be 0.
+    //
+    // In this situation the first item needs to start where it originated, at the top
+    
     if ( ([self.collectionView numberOfItemsInSection:0] == 1) && (itemIndexPath.row == 0)  ){
         [attributes setCenter:[self calculateCenterForFirstItem]];
         [attributes setSize:self.itemSize];
         return attributes;
     }
     
-    // This is the new item, so we need to set its alpha, size, z-index and center
+    // This is a brand new item, so we need to set its alpha, size, z-index and center
+    
     [attributes setCenter:CGPointMake(self.collectionView.bounds.size.width/2, self.collectionView.bounds.size.height/2)];
     [attributes setAlpha:0.0f];
     [attributes setSize:CGSizeZero];
@@ -129,17 +136,24 @@
     
     // Check if this indexPath appears in the list of index paths being updated.
     // If it doesn't, we can don't need to setup the center point
+    
     NSIndexSet *indexSet = [self.indexPathsBeingUpdated indexesOfObjectsPassingTest:^BOOL(UICollectionViewUpdateItem *updateItem, NSUInteger idx, BOOL *stop) {
         return (updateItem.indexPathBeforeUpdate.row == itemIndexPath.row);
     }];
     
-    // This item isn't one that's disappearing, therefore we only need to set the alpha attribute
+    // This item isn't one that's disappearing, therefore we can use the superclass attributes
+    
     if ([indexSet count] == 0) {
         return [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
     }
     
     // Test to see if we're handling the removal of the first item as it moves to make
-    // way for the second one
+    // way for the second one.  In this case, there will be 2 items, and the handling indexPath.row
+    // of the item we're dealing with will be 0
+    //
+    // In this scenario, the item needs to end up back at the top center as the
+    // only one
+    
     if ( ([self.collectionView numberOfItemsInSection:0] == 2) && (itemIndexPath.row == 0)  ){
         [attributes setCenter:[self calculateCenterForFirstItem]];
         [attributes setSize:self.itemSize];
@@ -148,7 +162,9 @@
         return attributes;
     }
 
-    // This is the disappearing item, so we need to set its alpha, size, z-index and center
+    // This is a disappearing item, so we need to set its alpha, size, z-index and center
+    // so that it zooms into towards the centre
+    
     [attributes setCenter:CGPointMake(self.collectionView.bounds.size.width/2, self.collectionView.bounds.size.height/2)];
     [attributes setAlpha:0.0f];
     [attributes setSize:CGSizeZero];
@@ -166,11 +182,17 @@
 
 -(float)calculateSpokeRadius {
     
+    // Calculates the radius of the 'spoke' connecting the item's center and the
+    // center of the collectionView
+    
+    // Find out which is the shorter side, in case
+    // the collectionView's not square
     float shorterSide = fminf(self.collectionView.bounds.size.width, self.collectionView.bounds.size.height);
     
     float collectionViewAllowance = shorterSide / 2;
     float itemWidthAllowance = self.itemSize.width / 2;
     
+    // Adjust for side padding (if any)
     return (collectionViewAllowance - (itemWidthAllowance + self.sidePadding));
     
 }
@@ -178,9 +200,13 @@
 -(float)calculateRotationPerItem {
     
     // Shouldn't rotate if there's only one item
+    
     if ([self.collectionView numberOfItemsInSection:0] == 1) {
         return 0.0f;
     }
+    
+    // Otherwise, the rotation is given by 360 / number of items
+    // (or 2Pi / number of items as we're dealing with radians here)
     
     return ( 2 * M_PI / [self.collectionView numberOfItemsInSection:0]);
     
