e5dff798a76b8d6f32e9e45163cdc2aee27c9905
Tim Duckett <tim@timduckett.co.uk>
2014-07-13 17:44:15 +0200

    Zindex now set to ensure that new item is on top

--- CVInsertion/CustomLayout.m
+++ CVInsertion/CustomLayout.m
@@ -159,7 +159,7 @@
     [attributes setCenter:CGPointMake(self.collectionView.bounds.size.width/2, self.collectionView.bounds.size.height/2)];
     [attributes setAlpha:0.0f];
     [attributes setSize:self.itemSize];
-    [attributes setZIndex:0];
+    [attributes setZIndex:[self.collectionView numberOfItemsInSection:0]];
     
     return attributes;
     
